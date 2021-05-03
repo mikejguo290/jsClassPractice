26e8e7a87ed5ddcf2707920fe43e586563effe7a
mikejguo290 <mikejguo290@gmail.com>
2021-05-03 13:34:20 +0100

    tidied up commentary, challenge locations etc.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-/*Use the following Network class to track how much total data and how many users each network has:
+/* Challenge #1. Use the following Network class to track how much total data and how many users each network has:
 
 The properties of the Network class are:
 
 data: Total units of data supplied by the network
 users: Total numbers of users currently on the network
 Each user on average deducts 5 units from the network’s total data. To watch a movie you must connect to a network that has at least 10 remaining units of data.
+
+
+Add a method movieTime() to the Network class that returns true if there is enough data available to watch a movie, false if there isn’t.
+
+Example:
+
+const library = new Network(50, 9) 
+library.movieTime() // returns false
 
 */
 
@@ -21,16 +29,7 @@ class Network {
     }
   }
 
-/*  1. Add a method movieTime() to the Network class that returns true if there is enough data available to watch a movie, false if there isn’t.
-
-Example:
-
-const library = new Network(50, 9) 
-library.movieTime() // returns false
-*/
-
-/*
-A video game consists of two players floating using 100 helium balloons. The players shoot pellets at each other’s balloons and after 10 minutes the player with the most balloons left wins.
+/* challenge#2.  A video game consists of two players floating using 100 helium balloons. The players shoot pellets at each other’s balloons and after 10 minutes the player with the most balloons left wins.
 
 Write a game function balloonAttack that takes two Player instances, calculates the balloons left for each player after 10 minutes (using the hitsPerMinute property) 
 and returns the name of the winner. If the result is a tie, return the string 'Tie'.
@@ -71,5 +70,3 @@ const p1 = new Player('p1', 5);
 const p2 = new Player('p2', 2);
 const winner = balloonAttack(p1, p2);
 console.log(winner);
-
-  
