2f93a568b8c7f267a77b988e010cfc49419f375a
mikejguo290 <mikejguo290@gmail.com>
2021-05-03 12:57:49 +0100

    initialised challenge #2 with CodeCademy code.

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ Example:
 const library = new Network(50, 9) 
 library.movieTime() // returns false
 */
+
+/*
+A video game consists of two players floating using 100 helium balloons. The players shoot pellets at each other’s balloons and after 10 minutes the player with the most balloons left wins.
+
+Write a game function balloonAttack that takes two Player instances, calculates the balloons left for each player after 10 minutes (using the hitsPerMinute property) 
+and returns the name of the winner. If the result is a tie, return the string 'Tie'.
+*/
+
+class Player {
+    constructor(name, hitsPerMinute) {
+      this.name = name;
+      this.hitsPerMinute = hitsPerMinute;
+      this.balloonCount = 100;
+    }
+    status() {
+      console.log(`Player: ${this.name} -- Balllons Left: ${this.balloonCount}`)
+    }
+  }
+  
